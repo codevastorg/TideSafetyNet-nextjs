0f1115770b3159ac51c5b257b7e7e4158711d774
warrenshiv <warrenshiv@gmail.com>
2024-02-07 08:01:29 +0300

    Added Bootstrap and custom theme.scss file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
+import React from "react";
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        <div className="text-center">
+          <h1 className="display-4 mb-4">
+            Welcome to Next.js version of the TideSafetyNet with Bootstrap!
+          </h1>
+          <button className="btn btn-primary mt-3">Get Started</button>
+        </div>
+      </div>
   );
-}
+};
+
+export default Home;
