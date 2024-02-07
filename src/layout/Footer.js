bb9cc4e88f004e3c1ef2d2c937eba89b13f0a844
Kiokosvm <sammysticalfrisk@gmail.com>
2024-02-07 14:22:46 +0300

    Applied  enhanced styling and content, including a background image, gradient effect, centered footer content with social media icons, and a Follow us on statement, to the Footer component.

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,20 +1,33 @@
 import React from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Footer = () => {
   return (
-    <div className="contact-section">
-      <div className="overlay">
-        <footer className="footer">
+    <div className="footer-container">
+      <div className="footer-content">
+        <footer className="container py-4">
           <p className="infos">
             &copy; {new Date().getFullYear()}, Made by{" "}
             <a
-              href="https://github.com/startups-44r0n"
+              href="https://thestartups.wuaze.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               StartUps
             </a>
           </p>
+          <div className="social-icons">
+          <p>Follow us on:</p>
+            <a href="https://www.youtube.com/@startups44r0n" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}>
+              <i className="bi bi-youtube"></i>
+            </a>
+            <a href="https://twitter.com/startups44r0n" target="_blank" rel="noopener noreferrer" style={{ marginRight: '10px' }}>
+              <i className="bi bi-twitter-x"></i>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-instagram"></i>
+            </a>
+          </div>
         </footer>
       </div>
     </div>
