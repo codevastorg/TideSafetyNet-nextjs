e37ba62a284c34b99badd3edc6784c6c7c9210c9
Kiokosvm <sammysticalfrisk@gmail.com>
2024-02-07 16:06:56 +0300

    wueh,local man has tried!

--- src/components/landing/Hero.js
+++ src/components/landing/Hero.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { BsBoxArrowInLeft, BsPerson, BsDownload, BsSearch } from 'react-icons/bs';
 
 const HeroSection = () => (
   <section className="hero-section">
@@ -7,10 +8,30 @@ const HeroSection = () => (
       <h1>Welcome to TideSafetyNet</h1>
       <p>Real-time quality controlled sea level data is available for free.</p>
       <nav>
-        <Link legacyBehavior href="/logout"><a>Logout</a></Link>
-        <Link legacyBehavior href="/userInfo"><a>User Info</a></Link>
-        <Link legacyBehavior href="./TideSafetyNet.apk"><a>Download App</a></Link>
-        <Link legacyBehavior href="#records"><a>Search Tide Records</a></Link>
+        <Link legacyBehavior href="/logout">
+          <a className="button">
+            <BsBoxArrowInLeft className="icon" />
+            Logout
+          </a>
+        </Link>
+        <Link legacyBehavior href="/userInfo">
+          <a className="button">
+            <BsPerson className="icon" />
+            User Info
+          </a>
+        </Link>
+        <Link legacyBehavior href="./TideSafetyNet.apk">
+          <a className="button">
+            <BsDownload className="icon" />
+            Download App
+          </a>
+        </Link>
+        <Link legacyBehavior href="#records">
+          <a className="button">
+            <BsSearch className="icon" />
+            Search Tide Records
+          </a>
+        </Link>
       </nav>
     </header>
   </section>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/scss/theme.scss";
 // import "../../public/assets/css/dorang.css";
 import "../../public/assets/css/Footer.css";
+import "../../public/assets/css/Hero.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
