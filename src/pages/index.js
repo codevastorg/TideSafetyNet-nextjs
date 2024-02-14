4727a3f609b0454863e3bd825638907135a16ede
warrenshiv <warrenshiv@gmail.com>
2024-02-14 15:56:54 +0300

    Update home.js and homepage.js, add SearchSection component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import * as React from "react";
 import Layout from "@/layout/Layout";
 import HeroSection from "@/components/landing/Hero";
 import SearchSection from "@/components/landing/SearchSection";
+import Homepage from "@/components/landing/home"
 
 const Home = () => {
   return (
     <div>
-      <Layout>
-        <HeroSection />
-        <SearchSection />
-      </Layout>
+      <Homepage />
     </div>
   );
 };
